cf65fb686f3f615508d94a2b6664c3458b97ad60
akshay91 <akshay91@stanford.edu>
2022-04-26 16:34:33 +0530

    meta changes

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home({ stars }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Pause A Moment</title>
+        <title>{`Pause a Moment ${stars}`}</title>
         <link rel="icon" href="/favicon.ico" />
 
         {/* <link rel="canonical" href="__META_URL__" /> */}
@@ -19,7 +19,7 @@ export default function Home({ stars }) {
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         {/* <meta property="og:url" content="__META_OG_URL__" id="meta_og_url" /> */}
-        <meta property="og:title" content="Pause a Moment" id="meta_og_title" />
+        <meta property="og:title" content={`Pause a Moment ${stars}`} id="meta_og_title" />
         <meta property="og:description" content="The COVID-19 pandemic has taken a toll. Pause a moment to focus on your well-being." id="meta_og_description" />
         <meta property="og:image" content="https://storage.googleapis.com/pause-a-moment-api-dev-public-content/thumbnail/pam-full-logo.png" id="meta_og_image" />
         {/* <meta property="og:image" content="%PUBLIC_URL%/logo192.png" id="meta_og_image" /> */}
@@ -27,7 +27,7 @@ export default function Home({ stars }) {
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         {/* <meta property="twitter:url" content="__META_TWITTER_URL__" id="meta_twt_url" /> */}
-        <meta property="twitter:title" content="Pause a Moment" id="meta_twt_title" />
+        <meta property="twitter:title" content={`Pause a Moment ${stars}`} id="meta_twt_title" />
         <meta property="twitter:description" content="The COVID-19 pandemic has taken a toll. Pause a moment to focus on your well-being." id="meta_twt_description" />
         <meta property="twitter:image" content="https://storage.googleapis.com/pause-a-moment-api-dev-public-content/thumbnail/pam-full-logo.png" id="meta_twt_image" />
       </Head>
