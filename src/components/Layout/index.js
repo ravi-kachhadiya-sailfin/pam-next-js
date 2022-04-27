a0f06947b3674dc6dbabc9b9c263fed60daa68de
akshay91 <akshay91@stanford.edu>
2022-04-27 12:09:59 +0530

    changes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import { useContext, useState, useEffect } from 'react';
 import propTypes from "prop-types";
 import { useHistory } from 'react-router-dom';
 
-import NavBar from "src/components/navbar/NavBar";
+import NavBar from "src/components/Navbar/NavBar";
 import Footer from "src/components/footer/Footer";
 
 import { AppStoreContext } from "src/providers/AppStoreProvider";
